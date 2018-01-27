157123a983e7402bfc563486266ed9ff487d8b99
Lebranc Convas <lebrancconvas@gmail.com>
2018-01-28 06:54:04 +0700

    Declare Variable and Print Var Testing.

---
+++ varprint.go
@@ -0,0 +1,10 @@
+package main 
+import "fmt"
+func main(){
+	var myoshi_number int = 5
+	fmt.Println(myoshi_number)
+	myoshi := "Cherprang"
+	fmt.Println(myoshi)
+	var oshi string = "Isurina"
+	fmt.Println("My oshi is",oshi)
+}
